b589fa81b28ed7aaaf0876554dd4d4e9d8aa4160
LisaBli <lisablindenhoefer@gmail.com>
2022-10-12 15:57:14 +0200

    aufgaben

---
+++ Aufgaben2.Semester/A01_RandomPoem/a01.js
@@ -0,0 +1,7 @@
+let subjekt = ["Die Katze", "Der Hund", "Jeder", "Anna", "Hans-Peter", "Der Prof"];
+let praedikat = ["zerfetzt", "sammelt", "reitet", "hasst", "streichelt", "pflegt"];
+let objekt = ["Löwen", "Pilze", "Oliven", "Pferde", "Bäume", "Nilpferde"];
+console.log(subjekt);
+console.log(praedikat);
+console.log(objekt);
+//# sourceMappingURL=a01.js.map
---
+++ Aufgaben2.Semester/A01_RandomPoem/a01.ts
@@ -0,0 +1,8 @@
+
+let subjekt: string[] = ["Die Katze", "Der Hund", "Jeder", "Anna", "Hans-Peter", "Der Prof"];
+let praedikat: string[] = ["zerfetzt", "sammelt", "reitet", "hasst", "streichelt", "pflegt"];
+let objekt: string[] = ["Löwen", "Pilze", "Oliven", "Pferde", "Bäume", "Nilpferde"];
+
+console.log(subjekt);
+console.log(praedikat);
+console.log(objekt);
