5dddaf6c756c592dc41c4269d9d5bb73a9195ae5
LisaBli <lisablindenhoefer@gmail.com>
2022-10-13 11:52:11 +0200

    aufgabe

--- Aufgaben2.Semester/A01_RandomPoem/a01.js
+++ Aufgaben2.Semester/A01_RandomPoem/a01.js
@@ -1,7 +1,23 @@
-let subjekt = ["Die Katze", "Der Hund", "Jeder", "Anna", "Hans-Peter", "Der Prof"];
-let praedikat = ["zerfetzt", "sammelt", "reitet", "hasst", "streichelt", "pflegt"];
-let objekt = ["Löwen", "Pilze", "Oliven", "Pferde", "Bäume", "Nilpferde"];
-console.log(subjekt);
-console.log(praedikat);
-console.log(objekt);
+let subjekte = ["Die Katze", "Der Hund", "Jeder", "Anna", "Hans-Peter", "Der Prof"];
+let praedikate = ["schlägt", "sammelt", "bewundert", "hasst", "liebt", "pflegt"];
+let objekte = ["Löwen", "Pilze", "Oliven", "Pferde", "Bäume", "Nilpferde"];
+console.log(subjekte);
+console.log(praedikate);
+console.log(objekte);
+//gibt Zufallsgedicht aus
+for (let _index = subjekte.length; _index > 0; _index--) {
+    getVerse(subjekte, praedikate, objekte);
+}
+//Funktion holt random Wörter aus Array und hängt sie aneinander 
+function getVerse(_subjekte, _praedikate, _objekte) {
+    let getsubjekt = Math.floor(Math.random() * subjekte.length);
+    let getpraedikat = Math.floor(Math.random() * praedikate.length);
+    let getobjekt = Math.floor(Math.random() * praedikate.length);
+    let s = _subjekte.splice(getsubjekt, 1);
+    let p = _praedikate.splice(getpraedikat, 1);
+    let o = _objekte.splice(getobjekt, 1);
+    let zeile = s + " " + p + " " + o + ".";
+    console.log(zeile);
+    return;
+}
 //# sourceMappingURL=a01.js.map
--- Aufgaben2.Semester/A01_RandomPoem/a01.ts
+++ Aufgaben2.Semester/A01_RandomPoem/a01.ts
@@ -1,8 +1,29 @@
 
-let subjekt: string[] = ["Die Katze", "Der Hund", "Jeder", "Anna", "Hans-Peter", "Der Prof"];
-let praedikat: string[] = ["zerfetzt", "sammelt", "reitet", "hasst", "streichelt", "pflegt"];
-let objekt: string[] = ["Löwen", "Pilze", "Oliven", "Pferde", "Bäume", "Nilpferde"];
+let subjekte: string[] = ["Die Katze", "Der Hund", "Jeder", "Anna", "Hans-Peter", "Der Prof"];
+let praedikate: string[] = ["schlägt", "sammelt", "bewundert", "hasst", "liebt", "pflegt"];
+let objekte: string[] = ["Löwen", "Pilze", "Oliven", "Pferde", "Bäume", "Nilpferde"];
 
-console.log(subjekt);
-console.log(praedikat);
-console.log(objekt);
+console.log(subjekte);
+console.log(praedikate);
+console.log(objekte);
+
+//gibt Zufallsgedicht aus
+for (let _index: number = subjekte.length; _index > 0; _index--) {
+    getVerse(subjekte, praedikate, objekte);
+}
+
+//Funktion holt random Wörter aus Array und hängt sie aneinander 
+function getVerse(_subjekte: string[], _praedikate: string[], _objekte: string[]):string {
+    
+    let getsubjekt: number = Math.floor(Math.random() * subjekte.length);
+    let getpraedikat: number = Math.floor(Math.random() * praedikate.length);
+    let getobjekt: number = Math.floor(Math.random() * praedikate.length);
+
+    let s: string[] = _subjekte.splice(getsubjekt, 1);
+    let p: string[] = _praedikate.splice(getpraedikat, 1);
+    let o: string[] = _objekte.splice(getobjekt, 1);
+
+    let zeile: string = s + " " + p + " " + o + ".";
+    console.log(zeile)
+    return;
+}
