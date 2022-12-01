a4951d191ddc7d724e61e04e048a39ef1a03e4c0
LisaBli <lisablindenhoefer@gmail.com>
2022-12-01 15:04:19 +0100

    aufgabe8

--- Aufgaben2.Semester/a08_vogelhaus/a08.js
+++ Aufgaben2.Semester/a08_vogelhaus/a08.js
@@ -22,8 +22,164 @@ var a08_2_Vogelhaus;
         drawCloud({ x: 500, y: 125 }, { x: 250, y: 75 });
         drawmountains(mountains, 75, 200, "grey", "white");
         drawmountains(mountains, 50, 150, "grey", "lightgrey");
+        drawtrees({ x: 200, y: 420 });
         drawsnowman({ x: 90, y: 620 });
         drawhouse({ x: 800, y: 540 });
+        drawbird();
+        drawsnowflakes();
+    }
+    function drawBackground() {
+        console.log("Background");
+        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(golden, "white");
+        gradient.addColorStop(1, "#b4c3d7");
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+    function drawSun(_position) {
+        console.log("sun", _position);
+        let r1 = 40;
+        let r2 = 150;
+        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
+        gradient.addColorStop(1, "HSLA(50, 100%, 40%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawCloud(_position, _size) {
+        console.log("Cloud", _position, _size);
+        let nParticles = 40;
+        let rparticle = 60;
+        let particle = new Path2D();
+        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, rparticle);
+        particle.arc(0, 0, rparticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x = (Math.random() - 0.5) * _size.x;
+            let y = -(Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    function drawmountains(_position, _min, _max, _colorLow, _colorHigh) {
+        console.log("Mountains", _position, _min, _max);
+        let stepMin = 50;
+        let stepMax = 150;
+        let x = 0;
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -_max);
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y = -_min - Math.random() * (_max - _min);
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+        let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawtrees(_position) {
+        console.log("trees");
+        crc2.save();
+        crc2.shadowBlur = 3;
+        crc2.shadowColor = "black";
+        crc2.beginPath();
+        crc2.translate(_position.x, _position.y);
+        crc2.scale(2, 2);
+        crc2.moveTo(20, 0);
+        crc2.lineTo(-20, 0);
+        crc2.lineTo(-15, -15);
+        crc2.lineTo(-18, -15);
+        crc2.lineTo(-10, -36);
+        crc2.lineTo(-12, -36);
+        crc2.lineTo(-5, -54);
+        crc2.lineTo(-7, -54);
+        crc2.lineTo(0, -73);
+        crc2.lineTo(7, -54);
+        crc2.lineTo(5, -54);
+        crc2.lineTo(12, -36);
+        crc2.lineTo(10, -36);
+        crc2.lineTo(18, -15);
+        crc2.lineTo(15, -15);
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.closePath();
+        crc2.restore();
+        crc2.save();
+        crc2.shadowBlur = 3;
+        crc2.shadowColor = "black";
+        crc2.beginPath();
+        crc2.translate(_position.x, _position.y);
+        crc2.moveTo(5, 0);
+        crc2.lineTo(-5, 0);
+        crc2.lineTo(-5, 19);
+        crc2.lineTo(5, 19);
+        crc2.fillStyle = "brown";
+        crc2.fill();
+        crc2.closePath();
+        crc2.restore();
+        for (let index = 0; index < 7; index++) {
+            let randomx = Math.floor(Math.random() * (700 - 200) + 200);
+            let randomy = Math.floor(Math.random() * (600 - 420) + 420);
+            let randomscale = Math.floor(Math.random() * (3 - 1) + 1);
+            crc2.save();
+            crc2.shadowBlur = 3;
+            crc2.shadowColor = "black";
+            crc2.beginPath();
+            crc2.translate(randomx, randomy);
+            crc2.scale(randomscale, randomscale);
+            crc2.moveTo(20, 0);
+            crc2.lineTo(-20, 0);
+            crc2.lineTo(-15, -15);
+            crc2.lineTo(-18, -15);
+            crc2.lineTo(-10, -36);
+            crc2.lineTo(-12, -36);
+            crc2.lineTo(-5, -54);
+            crc2.lineTo(-7, -54);
+            crc2.lineTo(0, -73);
+            crc2.lineTo(7, -54);
+            crc2.lineTo(5, -54);
+            crc2.lineTo(12, -36);
+            crc2.lineTo(10, -36);
+            crc2.lineTo(18, -15);
+            crc2.lineTo(15, -15);
+            crc2.fillStyle = "darkgreen";
+            crc2.fill();
+            crc2.closePath();
+            crc2.restore();
+            crc2.save();
+            crc2.shadowBlur = 3;
+            crc2.shadowColor = "black";
+            crc2.beginPath();
+            crc2.translate(randomx, randomy);
+            crc2.moveTo(5, 0);
+            crc2.lineTo(-5, 0);
+            crc2.lineTo(-5, 19);
+            crc2.lineTo(5, 19);
+            crc2.fillStyle = "brown";
+            crc2.fill();
+            crc2.closePath();
+            crc2.restore();
+        }
     }
     function drawsnowman(_position) {
         console.log("snowman", _position);
@@ -117,13 +273,13 @@ var a08_2_Vogelhaus;
         crc2.save();
         crc2.beginPath();
         crc2.translate(_position.x, _position.y);
-        crc2.moveTo(-90, 0);
-        crc2.lineTo(90, 0);
-        crc2.lineTo(90, -120);
+        crc2.moveTo(-80, 0);
+        crc2.lineTo(80, 0);
+        crc2.lineTo(80, -120);
         crc2.lineTo(120, -120);
         crc2.lineTo(0, -180);
         crc2.lineTo(-120, -120);
-        crc2.lineTo(-90, -120);
+        crc2.lineTo(-80, -120);
         crc2.fillStyle = "brown";
         crc2.fill();
         crc2.closePath();
@@ -147,84 +303,40 @@ var a08_2_Vogelhaus;
         crc2.fill();
         crc2.closePath();
         crc2.restore();
-        let rhole = 28;
+        let rhole = 32;
         crc2.save();
         crc2.beginPath();
         crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = "darkgrey";
+        crc2.fillStyle = "wihite";
         crc2.arc(0, -60, rhole, 0, 2 * Math.PI);
         crc2.fill();
         crc2.closePath();
         crc2.restore();
     }
-    function drawBackground() {
-        console.log("Background");
-        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "lightblue");
-        gradient.addColorStop(golden, "white");
-        gradient.addColorStop(1, "#b4c3d7");
-        crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    function drawbird() {
+        console.log("birds");
     }
-    function drawSun(_position) {
-        console.log("sun", _position);
-        let r1 = 40;
-        let r2 = 150;
-        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
-        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
-        gradient.addColorStop(1, "HSLA(50, 100%, 40%, 0)");
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = gradient;
-        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.restore();
-    }
-    function drawCloud(_position, _size) {
-        console.log("Cloud", _position, _size);
-        let nParticles = 40;
-        let rparticle = 60;
-        let particle = new Path2D();
-        let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, rparticle);
-        particle.arc(0, 0, rparticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
-        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = gradient;
-        for (let drawn = 0; drawn < nParticles; drawn++) {
+    function drawsnowflakes() {
+        console.log("snowflakes");
+        for (let index = 0; index < 600; index++) {
+            let randomx = Math.floor(Math.random() * (1000 - 1) + 1);
+            let randomy = Math.floor(Math.random() * (700 - 1) + 1);
+            let randomscale = Math.floor(Math.random() * (4 - 1) + 1);
+            let r1 = 1;
+            let r2 = 2;
+            let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+            gradient.addColorStop(0, "HSLA(0, 0%, 100%, 1)");
+            gradient.addColorStop(1, "HSLA(240, 50%, 90%, 0)");
             crc2.save();
-            let x = (Math.random() - 0.5) * _size.x;
-            let y = -(Math.random() * _size.y);
-            crc2.translate(x, y);
-            crc2.fill(particle);
+            crc2.scale(randomscale, randomscale);
+            crc2.beginPath();
+            crc2.translate(randomx, randomy);
+            crc2.fillStyle = gradient;
+            crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+            crc2.fill();
+            crc2.closePath();
             crc2.restore();
         }
-        crc2.restore();
-    }
-    function drawmountains(_position, _min, _max, _colorLow, _colorHigh) {
-        console.log("Mountains", _position, _min, _max);
-        let stepMin = 50;
-        let stepMax = 150;
-        let x = 0;
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.beginPath();
-        crc2.moveTo(0, 0);
-        crc2.lineTo(0, -_max);
-        do {
-            x += stepMin + Math.random() * (stepMax - stepMin);
-            let y = -_min - Math.random() * (_max - _min);
-            crc2.lineTo(x, y);
-        } while (x < crc2.canvas.width);
-        crc2.lineTo(x, 0);
-        crc2.closePath();
-        let gradient = crc2.createLinearGradient(0, 0, 0, -_max);
-        gradient.addColorStop(0, _colorLow);
-        gradient.addColorStop(0.7, _colorHigh);
-        crc2.fillStyle = gradient;
-        crc2.fill();
-        crc2.restore();
     }
 })(a08_2_Vogelhaus || (a08_2_Vogelhaus = {}));
 //# sourceMappingURL=a08.js.map
--- Aufgaben2.Semester/a08_vogelhaus/a08.ts
+++ Aufgaben2.Semester/a08_vogelhaus/a08.ts
@@ -33,10 +33,192 @@ namespace a08_2_Vogelhaus {
 
         drawmountains(mountains, 75, 200, "grey", "white");
         drawmountains(mountains, 50, 150, "grey", "lightgrey");
-
+        drawtrees({ x: 200, y: 420 });
         drawsnowman({ x: 90, y: 620 });
         drawhouse({ x: 800, y: 540 });
+        drawbird();
+        drawsnowflakes();
 
+    }
+
+    function drawBackground(): void {
+        console.log("Background");
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(golden, "white");
+        gradient.addColorStop(1, "#b4c3d7");
+
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    }
+
+    function drawSun(_position: Vector): void {
+        console.log("sun", _position);
+
+        let r1: number = 40;
+        let r2: number = 150;
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+
+        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
+        gradient.addColorStop(1, "HSLA(50, 100%, 40%, 0)");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.restore();
+    }
+
+
+    function drawCloud(_position: Vector, _size: Vector): void {
+        console.log("Cloud", _position, _size);
+
+        let nParticles: number = 40;
+        let rparticle: number = 60;
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, rparticle);
+
+        particle.arc(0, 0, rparticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x;
+            let y: number = - (Math.random() * _size.y);
+            crc2.translate(x, y);
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+
+    function drawmountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
+        console.log("Mountains", _position, _min, _max);
+        let stepMin: number = 50;
+        let stepMax: number = 150;
+        let x: number = 0;
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, -_max);
+
+        do {
+            x += stepMin + Math.random() * (stepMax - stepMin);
+            let y: number = -_min - Math.random() * (_max - _min);
+
+            crc2.lineTo(x, y);
+        } while (x < crc2.canvas.width);
+
+        crc2.lineTo(x, 0);
+        crc2.closePath();
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -_max);
+        gradient.addColorStop(0, _colorLow);
+        gradient.addColorStop(0.7, _colorHigh);
+
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.restore();
+    }
+
+    function drawtrees(_position: Vector): void {
+        console.log("trees");
+
+        crc2.save();
+        crc2.shadowBlur = 3;
+        crc2.shadowColor = "black";
+        crc2.beginPath();
+        crc2.translate(_position.x, _position.y);
+        crc2.scale(2, 2);
+        crc2.moveTo(20, 0);
+        crc2.lineTo(-20, 0);
+        crc2.lineTo(-15, -15);
+        crc2.lineTo(-18, -15);
+        crc2.lineTo(-10, -36);
+        crc2.lineTo(-12, -36);
+        crc2.lineTo(-5, -54);
+        crc2.lineTo(-7, -54);
+        crc2.lineTo(0, -73);
+        crc2.lineTo(7, -54);
+        crc2.lineTo(5, -54);
+        crc2.lineTo(12, -36);
+        crc2.lineTo(10, -36);
+        crc2.lineTo(18, -15);
+        crc2.lineTo(15, -15);
+        crc2.fillStyle = "darkgreen";
+        crc2.fill();
+        crc2.closePath();
+        crc2.restore();
+
+        crc2.save();
+        crc2.shadowBlur = 3;
+        crc2.shadowColor = "black";
+        crc2.beginPath();
+        crc2.translate(_position.x, _position.y);
+        crc2.moveTo(5, 0);
+        crc2.lineTo(-5, 0);
+        crc2.lineTo(-5, 19);
+        crc2.lineTo(5, 19);
+        crc2.fillStyle = "brown";
+        crc2.fill();
+        crc2.closePath();
+        crc2.restore();
+
+        for (let index: number = 0; index < 7; index++) {
+            let randomx: number = Math.floor(Math.random() * (700 - 200) + 200);
+            let randomy: number = Math.floor(Math.random() * (600 - 420) + 420);
+            let randomscale: number = Math.floor(Math.random() * (3 - 1) + 1);
+
+            crc2.save();
+            crc2.shadowBlur = 3;
+            crc2.shadowColor = "black";
+            crc2.beginPath();
+            crc2.translate(randomx, randomy);
+            crc2.scale(randomscale, randomscale);
+            crc2.moveTo(20, 0);
+            crc2.lineTo(-20, 0);
+            crc2.lineTo(-15, -15);
+            crc2.lineTo(-18, -15);
+            crc2.lineTo(-10, -36);
+            crc2.lineTo(-12, -36);
+            crc2.lineTo(-5, -54);
+            crc2.lineTo(-7, -54);
+            crc2.lineTo(0, -73);
+            crc2.lineTo(7, -54);
+            crc2.lineTo(5, -54);
+            crc2.lineTo(12, -36);
+            crc2.lineTo(10, -36);
+            crc2.lineTo(18, -15);
+            crc2.lineTo(15, -15);
+            crc2.fillStyle = "darkgreen";
+            crc2.fill();
+            crc2.closePath();
+            crc2.restore();
+
+            crc2.save();
+            crc2.shadowBlur = 3;
+            crc2.shadowColor = "black";
+            crc2.beginPath();
+            crc2.translate(randomx, randomy);
+            crc2.moveTo(5, 0);
+            crc2.lineTo(-5, 0);
+            crc2.lineTo(-5, 19);
+            crc2.lineTo(5, 19);
+            crc2.fillStyle = "brown";
+            crc2.fill();
+            crc2.closePath();
+            crc2.restore();
+        }
     }
 
     function drawsnowman(_position: Vector): void {
@@ -144,13 +326,13 @@ namespace a08_2_Vogelhaus {
         crc2.save();
         crc2.beginPath();
         crc2.translate(_position.x, _position.y);
-        crc2.moveTo(-90, 0);
-        crc2.lineTo(90, 0);
-        crc2.lineTo(90, -120);
+        crc2.moveTo(-80, 0);
+        crc2.lineTo(80, 0);
+        crc2.lineTo(80, -120);
         crc2.lineTo(120, -120);
         crc2.lineTo(0, -180);
         crc2.lineTo(-120, -120);
-        crc2.lineTo(-90, -120);
+        crc2.lineTo(-80, -120);
         crc2.fillStyle = "brown";
         crc2.fill();
         crc2.closePath();
@@ -176,107 +358,47 @@ namespace a08_2_Vogelhaus {
         crc2.closePath();
         crc2.restore();
 
-        let rhole: number = 28;
+        let rhole: number = 32;
         crc2.save();
         crc2.beginPath();
         crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = "darkgrey";
+        crc2.fillStyle = "wihite";
         crc2.arc(0, -60, rhole, 0, 2 * Math.PI);
         crc2.fill();
         crc2.closePath();
         crc2.restore();
     }
 
-    function drawBackground(): void {
-        console.log("Background");
-
-        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
-        gradient.addColorStop(0, "lightblue");
-        gradient.addColorStop(golden, "white");
-        gradient.addColorStop(1, "#b4c3d7");
-
-        crc2.fillStyle = gradient;
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    function drawbird(): void {
+        console.log("birds");
     }
 
-    function drawSun(_position: Vector): void {
-        console.log("sun", _position);
+    function drawsnowflakes(): void {
+        console.log("snowflakes");
 
-        let r1: number = 40;
-        let r2: number = 150;
-        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
+        for (let index: number = 0; index < 600; index++) {
+            let randomx: number = Math.floor(Math.random() * (1000 - 1) + 1);
+            let randomy: number = Math.floor(Math.random() * (700 - 1) + 1);
+            let randomscale: number = Math.floor(Math.random() * (4 - 1) + 1);
 
-        gradient.addColorStop(0, "HSLA(60, 100%, 90%, 1)");
-        gradient.addColorStop(1, "HSLA(50, 100%, 40%, 0)");
+            let r1: number = 1;
+            let r2: number = 2;
 
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = gradient;
-        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.restore();
-    }
+            let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
 
+            gradient.addColorStop(0, "HSLA(0, 0%, 100%, 1)");
+            gradient.addColorStop(1, "HSLA(240, 50%, 90%, 0)");
 
-    function drawCloud(_position: Vector, _size: Vector): void {
-        console.log("Cloud", _position, _size);
-
-        let nParticles: number = 40;
-        let rparticle: number = 60;
-        let particle: Path2D = new Path2D();
-        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, rparticle);
-
-        particle.arc(0, 0, rparticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
-        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
-
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = gradient;
-
-        for (let drawn: number = 0; drawn < nParticles; drawn++) {
             crc2.save();
-            let x: number = (Math.random() - 0.5) * _size.x;
-            let y: number = - (Math.random() * _size.y);
-            crc2.translate(x, y);
-            crc2.fill(particle);
+            crc2.scale(randomscale, randomscale);
+            crc2.beginPath();
+            crc2.translate(randomx, randomy);
+            crc2.fillStyle = gradient;
+            crc2.arc(0, 0, r2, 0, 2 * Math.PI);
+            crc2.fill();
+            crc2.closePath();
             crc2.restore();
         }
-        crc2.restore();
     }
-
-    function drawmountains(_position: Vector, _min: number, _max: number, _colorLow: string, _colorHigh: string): void {
-        console.log("Mountains", _position, _min, _max);
-        let stepMin: number = 50;
-        let stepMax: number = 150;
-        let x: number = 0;
-
-        crc2.save();
-        crc2.translate(_position.x, _position.y);
-
-        crc2.beginPath();
-        crc2.moveTo(0, 0);
-        crc2.lineTo(0, -_max);
-
-        do {
-            x += stepMin + Math.random() * (stepMax - stepMin);
-            let y: number = -_min - Math.random() * (_max - _min);
-
-            crc2.lineTo(x, y);
-        } while (x < crc2.canvas.width);
-
-        crc2.lineTo(x, 0);
-        crc2.closePath();
-
-        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, -_max);
-        gradient.addColorStop(0, _colorLow);
-        gradient.addColorStop(0.7, _colorHigh);
-
-        crc2.fillStyle = gradient;
-        crc2.fill();
-
-        crc2.restore();
-    }
-
 
 }
