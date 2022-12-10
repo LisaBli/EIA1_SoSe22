fb3737c63f94adac46de2a2ac4419eb861fec655
LisaBli <lisablindenhoefer@gmail.com>
2022-12-10 13:36:55 +0100

    9

--- Aufgaben2.Semester/a09_OldMacDonaldsFarm/a09.js
+++ Aufgaben2.Semester/a09_OldMacDonaldsFarm/a09.js
@@ -3,7 +3,7 @@ Aufgabe: 09.1
 Name: Lisa Blindenhöfer
 Matrikel Nr.: 271450
 Datum: 10.12.2022
-Quellen: EIA-Videos Lektion 09, Bastian Aberle
+Quellen: EIA-Videos Lektion 09, W3-Schools, Bastian Aberle
 */
 var a09OldMcdonalds;
 (function (a09OldMcdonalds) {
@@ -19,84 +19,83 @@ var a09OldMcdonalds;
         food;
         amount;
         sound;
-        sounds;
-        constructor(_name, _species, _food, _amount, _sound, _sounds) {
+        sound2;
+        constructor(_name, _species, _food, _amount, _sound, _sound2) {
             this.name = _name;
             this.species = _species;
             this.food = _food;
             this.amount = _amount;
             this.sound = _sound;
-            this.sounds = _sounds;
+            this.sound2 = _sound2;
         }
         sing() {
-            let output = document.getElementById("divoutput");
+            let divoutput = document.getElementById("divoutput");
             let newDiv = document.createElement("div");
-            newDiv.id = "divsong";
             newDiv.innerHTML = "<br>" + this.name + "<br>" + "<br>" + "OldMac Donald had a farm" + "<br>"
                 + "Ee i ee i o" + "<br>"
                 + "And on his farm he had some " + this.species + "<br>"
-                + "With a " + this.sounds + " here" + "<br>"
-                + "And a " + this.sounds + " there" + "<br>"
+                + "With a " + this.sound2 + " here" + "<br>"
+                + "And a " + this.sound2 + " there" + "<br>"
                 + "Here a " + this.sound + "<br>"
                 + " there a " + this.sound + "<br>"
-                + "Everywhere a " + this.sounds + "<br>" + "<br>";
-            output.appendChild(newDiv);
+                + "Everywhere a " + this.sound2 + "<br>" + "<br>";
+            divoutput.appendChild(newDiv);
         }
         eat() {
             if (this.species == "cows") {
                 console.log("eat");
                 amountHay = amountHay - this.amount;
-                let output = document.getElementById("divoutput");
+                let divoutput = document.getElementById("divoutput");
                 let newDiv = document.createElement("div");
                 newDiv.innerHTML = amountHay + " hay: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
             if (this.species == "horses") {
                 amountCarots = amountCarots - this.amount;
-                let output = document.getElementById("divoutput");
+                let divoutput = document.getElementById("divoutput");
                 let newDiv = document.createElement("div");
                 newDiv.innerHTML = amountCarots + " carots: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
             if (this.species == "dogs") {
                 amountMeat = amountMeat - this.amount;
-                let output = document.getElementById("divoutput");
+                let divoutput = document.getElementById("divoutput");
                 let newDiv = document.createElement("div");
                 newDiv.innerHTML = amountMeat + " meats: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
             if (this.species == "cats") {
                 amountFish = amountFish - this.amount;
-                let output = document.getElementById("divoutput");
+                let divoutput = document.getElementById("divoutput");
                 let newDiv = document.createElement("div");
                 newDiv.innerHTML = amountFish + " fish: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
             if (this.species == "pigs") {
                 amountCorn = amountCorn - this.amount;
-                let output = document.getElementById("divoutput");
+                let divoutput = document.getElementById("divoutput");
                 let newDiv = document.createElement("div");
                 newDiv.innerHTML = amountCorn + " corn: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
         }
     }
-    let animal1 = new Animal("Rudolph", "cows", "hay", 20, "moo", "moo-moo");
-    let animal2 = new Animal("Donner", "horses", "carots", 4, "wieher", "wieh-wieh");
-    let animal3 = new Animal("Blitz", "dogs", "meat", 3, "woof", "woof-woof");
-    let animal4 = new Animal("Comet", "cats", "fish", 2, "miau", "miau-miau");
-    let animal5 = new Animal("Cupid", "pigs", "corn", 10, "oink", "oink-oink");
+    let cows = new Animal("Rudolph", "cows", "hay", 20, "moo", "moo-moo");
+    let horses = new Animal("Donner", "horses", "carots", 4, "wieher", "wieh-wieh");
+    let dogs = new Animal("Blitz", "dogs", "meat", 3, "woof", "woof-woof");
+    let cats = new Animal("Comet", "cats", "fish", 2, "miau", "miau-miau");
+    let pigs = new Animal("Cupid", "pigs", "corn", 10, "oink", "oink-oink");
     function handleload() {
-        animal1.sing();
-        animal1.eat();
-        animal2.sing();
-        animal2.eat();
-        animal3.sing();
-        animal3.eat();
-        animal4.sing();
-        animal4.eat();
-        animal5.sing();
-        animal5.eat();
+        cows.sing();
+        cows.eat();
+        horses.sing();
+        horses.eat();
+        dogs.sing();
+        dogs.eat();
+        cats.sing();
+        cats.eat();
+        pigs.sing();
+        pigs.eat();
     }
 })(a09OldMcdonalds || (a09OldMcdonalds = {}));
 //# sourceMappingURL=a09.js.map
--- Aufgaben2.Semester/a09_OldMacDonaldsFarm/a09.ts
+++ Aufgaben2.Semester/a09_OldMacDonaldsFarm/a09.ts
@@ -3,7 +3,7 @@ Aufgabe: 09.1
 Name: Lisa Blindenhöfer
 Matrikel Nr.: 271450
 Datum: 10.12.2022
-Quellen: EIA-Videos Lektion 09, Bastian Aberle
+Quellen: EIA-Videos Lektion 09, W3-Schools, Bastian Aberle
 */
 
 
@@ -18,95 +18,99 @@ namespace a09OldMcdonalds {
     let amountCorn: number = 200;
 
     class Animal {
+
         name: string;
         species: string;
         food: string;
         amount: number;
         sound: string;
-        sounds: string;
+        sound2: string;
 
-        constructor(_name: string, _species: string, _food: string, _amount: number, _sound: string, _sounds: string) {
+        constructor(_name: string, _species: string, _food: string, _amount: number, _sound: string, _sound2: string) {
             this.name = _name;
             this.species = _species;
             this.food = _food;
             this.amount = _amount;
             this.sound = _sound;
-            this.sounds = _sounds;
+            this.sound2 = _sound2;
         }
 
         sing(): void {
-            let output: HTMLElement = document.getElementById("divoutput");
+            let divoutput: HTMLElement = document.getElementById("divoutput");
             let newDiv: HTMLDivElement = document.createElement("div");
-            newDiv.id = "divsong";
             newDiv.innerHTML = "<br>" + this.name + "<br>" + "<br>" + "OldMac Donald had a farm" + "<br>" 
             + "Ee i ee i o" + "<br>" 
             + "And on his farm he had some " + this.species + "<br>" 
-            + "With a " + this.sounds + " here" + "<br>" 
-            + "And a " + this.sounds + " there" + "<br>" 
+            + "With a " + this.sound2 + " here" + "<br>" 
+            + "And a " + this.sound2 + " there" + "<br>" 
             + "Here a " + this.sound + "<br>" 
             + " there a " + this.sound + "<br>" 
-            + "Everywhere a " + this.sounds + "<br>" + "<br>";
-            output.appendChild(newDiv);
+            + "Everywhere a " + this.sound2 + "<br>" + "<br>";
+            divoutput.appendChild(newDiv);
         }
 
         eat(): void {
             if (this.species == "cows") {
                 console.log("eat");
                 amountHay = amountHay - this.amount;
-                let output: HTMLElement = document.getElementById("divoutput");
+                let divoutput: HTMLElement = document.getElementById("divoutput");
                 let newDiv: HTMLDivElement = document.createElement("div");
                 newDiv.innerHTML = amountHay + " hay: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
 
             }
             if (this.species == "horses") {
                 amountCarots = amountCarots - this.amount;
-                let output: HTMLElement = document.getElementById("divoutput");
+                let divoutput: HTMLElement = document.getElementById("divoutput");
                 let newDiv: HTMLDivElement = document.createElement("div");
                 newDiv.innerHTML = amountCarots + " carots: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
   
             }
             if (this.species == "dogs") {
                 amountMeat = amountMeat - this.amount;
-                let output: HTMLElement = document.getElementById("divoutput");
+                let divoutput: HTMLElement = document.getElementById("divoutput");
                 let newDiv: HTMLDivElement = document.createElement("div");
                 newDiv.innerHTML = amountMeat + " meats: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
             if (this.species == "cats") {
                 amountFish = amountFish - this.amount;
-                let output: HTMLElement = document.getElementById("divoutput");
+                let divoutput: HTMLElement = document.getElementById("divoutput");
                 let newDiv: HTMLDivElement = document.createElement("div");
                 newDiv.innerHTML = amountFish + " fish: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
             if (this.species == "pigs") {
                 amountCorn = amountCorn - this.amount;
-                let output: HTMLElement = document.getElementById("divoutput");
+                let divoutput: HTMLElement = document.getElementById("divoutput");
                 let newDiv: HTMLDivElement = document.createElement("div");
                 newDiv.innerHTML = amountCorn + " corn: left over" + "<br>" + "<br>" + "<br>";
-                output.appendChild(newDiv);
+                divoutput.appendChild(newDiv);
             }
         }
     }
 
-    let animal1: Animal = new Animal("Rudolph", "cows", "hay", 20, "moo", "moo-moo");
-    let animal2: Animal = new Animal("Donner", "horses", "carots", 4, "wieher", "wieh-wieh");
-    let animal3: Animal = new Animal("Blitz", "dogs", "meat", 3, "woof", "woof-woof");
-    let animal4: Animal = new Animal("Comet", "cats", "fish", 2, "miau", "miau-miau");
-    let animal5: Animal = new Animal("Cupid", "pigs", "corn", 10, "oink", "oink-oink");
+    let cows: Animal = new Animal("Rudolph", "cows", "hay", 20, "moo", "moo-moo");
+    let horses: Animal = new Animal("Donner", "horses", "carots", 4, "wieher", "wieh-wieh");
+    let dogs: Animal = new Animal("Blitz", "dogs", "meat", 3, "woof", "woof-woof");
+    let cats: Animal = new Animal("Comet", "cats", "fish", 2, "miau", "miau-miau");
+    let pigs: Animal = new Animal("Cupid", "pigs", "corn", 10, "oink", "oink-oink");
 
     function handleload(): void {
-        animal1.sing();
-        animal1.eat();
-        animal2.sing();
-        animal2.eat();
-        animal3.sing();
-        animal3.eat();
-        animal4.sing();
-        animal4.eat();
-        animal5.sing();
-        animal5.eat();
+        cows.sing();
+        cows.eat();
+
+        horses.sing();
+        horses.eat();
+
+        dogs.sing();
+        dogs.eat();
+
+        cats.sing();
+        cats.eat();
+
+        pigs.sing();
+        pigs.eat();
     }
 }
